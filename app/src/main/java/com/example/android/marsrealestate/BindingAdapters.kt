6a0888d74cab17c1edbd37f5584530bf119433f0
Farukh Farkhatov <farkhatov_f@auca.kg>
2021-10-16 19:54:00 +0600

    Displayed first image

--- app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsrealestate/BindingAdapters.kt
@@ -1,4 +1,17 @@
 package com.example.android.marsrealestate
 
+import android.widget.ImageView
+import androidx.core.net.toUri
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 
+@BindingAdapter("imageUrl")
+fun bindImage(imgView: ImageView, imgUrl: String?) {
+    imgUrl?.let {
+        val imgUrl = it.toUri().buildUpon().scheme("https").build()
+        Glide.with(imgView.context)
+            .load(imgUrl)
+            .into(imgView)
+    }
+}
 
